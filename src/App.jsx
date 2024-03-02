422815aba55a79756974f00951a02d881d38d1bd
kimonkekes <kimonkekes1@gmail.com>
2024-03-02 11:33:57 +0200

    Front-end visual embellishment

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import ParticlesBg from 'particles-bg'
 import './App.css'
 
 function App() {
 
 
   return (
+    <>
     <div className='App'>
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
+    <ParticlesBg className='particles' type='cobweb' bg={true} />
+    </>
   )
 }
 
---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,14 @@
+const Rank= () => {
+    return (
+      <div>
+        <div className='white f3 center'>
+          {'User, your current rank is...'}
+        </div>
+        <div className='white f1 center'>
+          {'#5'}
+        </div>
+      </div>
+    )
+}
+
+export default Rank
