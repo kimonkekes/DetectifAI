5190a5f9a19fa3a4837ed6502ba64f42fe847357
kimonkekes <kimonkekes1@gmail.com>
2024-03-04 12:17:19 +0200

    Basic face recognition functional

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,79 @@ import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import ParticlesBg from 'particles-bg'
 import './App.css'
 
 function App() {
   const [input, setInput] = useState('')
+  const [imageURL, setImageURL] = useState('')
+  
+
+  
+  const PAT = ''
+  const USER_ID = ''
+  const APP_ID = 'image-detection'
+  const MODEL_ID = 'face-detection'
 
   const onInputChange = (event) => {
-    console.log(event.target.value)
+    setInput(event.target.value)
+    console.log(input)
   }
 
   const onButtonSubmit = () => {
-    console.log('click');
+    setImageURL(input)
+    console.log(imageURL)
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": input
+                  }
+              }
+          }
+      ]
+  });
+  
+  const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+  };
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+
+        const regions = result.outputs[0].data.regions;
+
+        regions.forEach(region => {
+            const boundingBox = region.region_info.bounding_box;
+            const topRow = boundingBox.top_row.toFixed(3);
+            const leftCol = boundingBox.left_col.toFixed(3);
+            const bottomRow = boundingBox.bottom_row.toFixed(3);
+            const rightCol = boundingBox.right_col.toFixed(3);
+
+            region.data.concepts.forEach(concept => {
+                const name = concept.name;
+                const value = concept.value.toFixed(4);
+
+                console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
+                
+            });
+        });
+
+    })
+    .catch(error => console.log('error', error));
   }
 
   return (
@@ -26,7 +87,7 @@ function App() {
       <ImageLinkForm 
         onInputChange={onInputChange} 
         onButtonSubmit={onButtonSubmit} />
-      {/* <FaceRecognition /> */}
+      <FaceRecognition imageURL={imageURL} />
     </div>
     <ParticlesBg className='particles' type='cobweb' bg={true} />
     </>
