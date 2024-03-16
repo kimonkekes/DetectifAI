7395604b4b70d2bfa8f35257da9837bd7d51dad5
kimonkekes <kimonkekes1@gmail.com>
2024-03-16 11:53:44 +0200

    Added project title

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,16 +1,21 @@
 import Tilt from 'react-parallax-tilt';
 import detLogo from '../../assets/detLogo.png'
+import detTitle from'../../assets/detTitle.png'
 
 
 const Logo = () => {
     return (
+      <div className='flex items-center'>
       <div className='ml5 w4'>
         <Tilt className='br2 shadow-2'>
           <div style={{ height: '150px', width: '150px', backgroundColor: 'darkgreen' }}>
             <img src = {detLogo} alt='logo'></img>
           </div>
         </Tilt>
-
+      </div>
+      <div className='ml5'>
+        <img src = {detTitle} alt='title'></img>
+      </div>
       </div>
     )
 }
